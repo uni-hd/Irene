b06d3e54c5b9c224790cd92eb5728714d34cdfa6
qwang <qwang-big>
2019-12-02 14:50:14 +0100

    update examples

--- inst/extdata/example.R
+++ inst/extdata/example.R
@@ -26,24 +26,15 @@ meta$condition=as.integer(meta$condition=="Healthy")+1
 dPCA(meta,rdata[[i]]$bed,rdata[[i]]$data,lambda=lx[i])
 })
 #ranks
-print('computing ranks 1/4 ...')
+print('computing ranks 1/2 ...')
 rank=lapply(1:7,function(i){
 spec=substr(confs[[i]]$ipReads,1,4)[1]
 hic=read.table(paste0('in.',spec,'.bed.gz'),sep='\t',col.names=c('seqnames','start','end','gene','enh'))
 g=make.graph(hic[,c('enh','gene')], res[[i]]$bed[,4], abs(res[[i]]$PC[,1]))
 page_rank(g$g, algo="arpack", personalized=g$v, directed=TRUE)$vector
 })
-#histone marks
-print('computing ranks 2/4 ...')
-marks=lapply(1:7,function(i){
-hic=read.table(paste0('in.',substr(confs[[i]]$ipReads,1,4)[1],'.bed.gz'),sep='\t',col.names=c('seqnames','start','end','gene','enh'))
-lapply(1:6,function(j){
-g=make.graph(hic[,c('enh','gene')], res[[i]]$bed[,4], abs(res[[i]]$Dobs[,j]))
-page_rank(g$g, algo="arpack", personalized=g$v, directed=TRUE)$vector
-})
-})
 #nearest enhancer
-print('computing ranks 3/4 ...')
+print('computing ranks 2/3 ...')
 nearest=lapply(1:7,function(i){
 spec=substr(confs[[i]]$ipReads,1,4)[1]
 hic=read.table(paste0('nearest.',spec,'.bed.gz'),sep='\t',col.names=c('seqnames','start','end','gene','enh'))
@@ -51,7 +42,7 @@ g=make.graph(hic[,c('enh','gene')], res[[i]]$bed[,4], abs(res[[i]]$PC[,1]))
 page_rank(g$g, algo="arpack", personalized=g$v, directed=TRUE)$vector
 })
 #rewired
-print('computing ranks 4/4 ...')
+print('computing ranks 3/3 ...')
 rewired=lapply(1:7,function(i){
 spec=substr(confs[[i]]$ipReads,1,4)[1]
 hic=read.table(paste0('in.',spec,'.bed.gz'),sep='\t',col.names=c('seqnames','start','end','gene','enh'))
@@ -89,30 +80,27 @@ v=c(2,5,6,4,1,7,3)
 hic=lapply(1:7,function(i) read.table(paste0('in.',substr(confs[[i]]$ipReads,1,4)[1],'.bed.gz'),sep='\t',col.names=c('seqnames','start','end','prom','enh')))
 dname=function(i) nm[v][i]
 ad <- function(x,y,z,d) data.frame(Type=x,Method=y,Genes=z,value=sort(d))
-auc=dframe(do.call(rbind,lapply(v,function(i)c(get.auc(get.rank(markers[[i]], get.generank(rank[[i]]))), unlist(lapply(1:6,function(j) get.auc(get.rank(markers[[i]], get.generank(marks[[i]][[j]])))))))),col.names=c('dPC1',mk),m.colnames=c('Type','Mark','value'),melt=T)
-ggplot(auc, aes(x=Type,y=value,colour=Mark,shape=Mark))+geom_point(size=3)+scale_shape_manual(values=c(19,17,12:8))+scale_color_manual(values=RColorBrewer::brewer.pal(7,"Dark2"))+ scale_x_continuous(breaks=1:7,label=dname)+ labs(x="",y="AUC")
-ggsave("f1.pdf",width=6,height=6,units="in")
 df=do.call(rbind,lapply(1:7,function(i) rbind(
 ad(nm[i],'Irene','Cancer marker genes',get.rank(markers[[i]], get.generank(rank[[i]]))), 
 ad(nm[i],'Irene','Housekeeping genes',get.rank(markers$HKG, get.generank(rank[[i]]))),
 ad(nm[i],'Promoter','Cancer marker genes',get.rank(markers[[i]], prom[[i]])),
 ad(nm[i],'Promoter','Housekeeping genes',get.rank(markers$HKG, prom[[i]])))))
-ggplot(df,aes(value, colour=Method, linetype=Genes))+stat_ecdf(pad=TRUE)+labs(x="Rank", y="ECDF") +facet_wrap(.~Type)+ theme(legend.position=c(0.8,0.14))
+p=ggplot(df,aes(value, colour=Method, linetype=Genes))+stat_ecdf(pad=TRUE)+labs(x="Rank", y="ECDF") +facet_wrap(.~Type)+ theme(legend.position=c(0.8,0.14))
 ggsave("f2.pdf",width=6,height=6,units="in")
 df=do.call(rbind,lapply(1:7,function(i) rbind(
 ad(nm[i],'Promoter','Cancer marker genes',get.generankscore(promrank[[i]])[markers[[i]]]),
 ad(nm[i],'Promoter','Housekeeping genes',get.generankscore(promrank[[i]])[markers$HKG]))))
-ggplot()+geom_boxplot(data=df, aes(Type, value, colour=Genes),outlier.shape = NA) +labs(x='', y="dPC1")+ theme(legend.position=c(.85,.9))
-ggsave("f6.pdf",width=6,height=6,units="in")
+p=ggplot()+geom_boxplot(data=df, aes(Type, value, colour=Genes),outlier.shape = NA) +labs(x='', y="dPC1")+ theme(legend.position=c(.85,.9))
+ggsave("f1.pdf",width=6,height=6,units="in")
 df=dframe(do.call(rbind,lapply(1:7,function(i)summary(prcomp(res[[i]]$Dobs))$importance[2,])),row.names=nm,col.names=paste0('dPC',1:6),m.colnames=c('Type','dPC','value'),melt=T)
-ggplot(df,aes(Type, value, fill=dPC))+geom_bar(stat="identity",position="stack")+labs(x='', y="Variance")+scale_y_continuous(labels = scales::percent) 
+p=ggplot(df,aes(Type, value, fill=dPC))+geom_bar(stat="identity",position="stack")+labs(x='', y="Variance")+scale_y_continuous(labels = scales::percent) 
 ggsave("f3.pdf",width=6,height=6,units="in")
 df = do.call(rbind,lapply(1:7, function(i) data.frame(melt(prcomp(res[[i]]$Dobs)$rotation),rep(mk,6),nm[i])))
 colnames(df)=c("id","PC","Loadings","Mark","type")
-ggplot(df, aes(x=Mark, y=Loadings, fill=Mark)) + geom_bar(stat='identity')+facet_grid(PC~type) +coord_flip()+theme(legend.position="none",axis.text.x = element_text(angle = 90, vjust = 0.5))
+p=ggplot(df, aes(x=Mark, y=Loadings, fill=Mark)) + geom_bar(stat='identity')+facet_grid(PC~type) +coord_flip()+theme(legend.position="none",axis.text.x = element_text(angle = 90, vjust = 0.5))
 ggsave("f4.pdf",width=6,height=6,units="in")
 df=dframe(do.call(rbind,lapply(v,function(i) unlist(lapply(1:10,function(j) get.auc(get.rank(markers[[i]], rewired[[i]][[j]])))))),row.names=nm[v],melt=T)
 au=dframe(do.call(rbind,lapply(v,function(i)c(get.auc(get.rank(markers[[i]], get.generank(rank[[i]]))),get.auc(get.rank(markers[[i]], prom[[i]])),get.auc(get.rank(markers[[i]], get.generank(nearest[[i]])))))),col.names=c('Irene','Promoter','Nearest'),m.colnames=c('Type','Method','value'),melt=T)
-ggplot() + geom_boxplot(data=df, aes(Var1, value),outlier.shape = NA) + geom_line(data=au, aes(Type, value, colour=Method))+ labs(x='',y="AUC")+ theme(legend.position=c(.85,.89))
+p=ggplot() + geom_boxplot(data=df, aes(Var1, value),outlier.shape = NA) + geom_line(data=au, aes(Type, value, colour=Method))+ labs(x='',y="AUC")+ theme(legend.position=c(.85,.89))
 ggsave("f5.pdf",width=6,height=6,units="in")
 
